f698b5c7838e752648e497bcfb70c35664d3543b
satoshiyr16 <satoshi.trip.1@gmail.com>
2023-07-21 00:41:47 +0900

    テスト

--- ShiftMgBlog/templates/Homes/index.php
+++ ShiftMgBlog/templates/Homes/index.php
@@ -11,6 +11,7 @@
       <p class="rest">休憩時間：<?= $shift->rest ?>時間</p>
       <h4 class="money">合計：<?= $shift->money ?></h4>
     <?php endforeach; ?>
+    <h1>テスト</h1>
     </div>
   </div>
 </body>
