f1aeb8e9da163464b5a8f5dc011fcb61646fafe2
satoshiyr16 <satoshi.trip.1@gmail.com>
2023-08-03 17:52:55 +0900

    cssでhomeのデザイン変更

--- ShiftMgBlog/templates/Homes/index.php
+++ ShiftMgBlog/templates/Homes/index.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <head>
+  <?= $this->Html->css('homepage.css') ?>
 </head>
 <body>
-  <div class="shift-area">
-    <div class="shift-content-area">
+  <div class="shift_area">
     <?php foreach($shifts as $shift): ?>
+    <div class="shift_content_area">
       <h3 class="company"><?= $shift->company ?></h3>
-      <p class="time">働いた時間：<?= $shift->time ?>時間</p>
-      <p class="rest">休憩時間：<?= $shift->rest ?>時間</p>
-      <p class="subtle">時給：<?= $shift->hourly_wage ?>円</p>
-      <h4 class="money">合計：<?= $shift->total_money ?>円</h4>
-    <?php endforeach; ?>
+      <div class="worked_content_area">
+        <div class="worked_content_column">
+          <p class="time column">働いた時間：<?= $shift->time ?>時間</p>
+          <p class="rest column">休憩時間：<?= $shift->rest ?>時間</p>
+        </div>
+        <div class="worked_content_column">
+          <p class="subtle column">時給：<?= $shift->hourly_wage ?>円</p>
+        </div>
+      </div>
+      <h4 class="total_money">合計：<?= $shift->total_money ?>円</h4>
     </div>
+    <?php endforeach; ?>
   </div>
 </body>
 </html>
--- ShiftMgBlog/templates/layout/default.php
+++ ShiftMgBlog/templates/layout/default.php
@@ -34,6 +34,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
+
 </head>
 <body>
     <nav class="top-nav">
